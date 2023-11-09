9f67e678d5bb85ab8619527affda834eb3858a0b
Max Edwards <maxedwards20@gmail.com>
2023-11-09 10:58:21 -0700

    update serial

--- serial.cpp
+++ serial.cpp
@@ -13,7 +13,7 @@ using namespace std;
  * @param numEpochs - number of k means iterations
  * @param centroids - pointer to vector of centroids
  */
-void kMeansClusteringParallelCPU(vector<Point3D> *points, int numEpochs, vector<Point3D> *centroids)
+void kMeansClusteringSerial(vector<Point3D> *points, int numEpochs, vector<Point3D> *centroids)
 {
   // Repeat over epochs to converge the centroids
   for (int i = 0; i < numEpochs; ++i)
@@ -47,7 +47,7 @@ void performSerial(int numEpochs, int numCentroids, vector<Point3D> *centroids, 
   // create centroids
   cout << "Entering the k means computation" << endl;
   auto start_time = std::chrono::high_resolution_clock::now();
-  kMeansClusteringParallelCPU(points, numEpochs, centroids); // K-means clustering on the points.
+  kMeansClusteringSerial(points, numEpochs, centroids); // K-means clustering on the points.
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
   printStats(numEpochs, numCentroids, points, duration.count());
