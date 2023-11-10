11327dfdddfd9249f1f20aca588340b30a6f6b61
Max Edwards <maxedwards20@gmail.com>
2023-11-10 08:24:35 -0700

    Updates data we are passing into distributed

--- serial-to-distributed-cpu.cpp
+++ serial-to-distributed-cpu.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void run(int numEpochs, int numCentroids, vector<Point3D> *points)
 {
-    // Because this is random initialization we need to share it between the serial and GPU to ensure they are valid
+    // Because this is random initialization we need to share it between the serial and distributed implementations
     vector<Point3D> centroids = initializeCentroids(numCentroids, points);
     // Copies the data to ensure we are validating correctly
     // https://www.geeksforgeeks.org/ways-copy-vector-c/
@@ -17,9 +17,18 @@ void run(int numEpochs, int numCentroids, vector<Point3D> *points)
     cout << "Performing Serial CPU" << endl;
     performSerial(numEpochs, &centroids, points, serialFilename);
     cout << "\nPerforming Distributed CPU " << endl;
-    performDistributedCPU(numEpochs, &centroids, &distributedPointsCopy, distributedFilename);
+    performDistributedCPU(numEpochs, &distributedCentroidCopy, &distributedPointsCopy, distributedFilename);
+
     // Compare outputs to validate they computed the same values
-    cout << "Files Equal: " << areFilesEqual(serialFilename, distributedFilename, true) << endl;
+    bool debug = false;
+    if (debug)
+    {
+        areFilesEqual(serialFilename, distributedFilename, debug)
+    }
+    else
+    {
+        cout << "Files Equal: " << areFilesEqual(serialFilename, distributedFilename, debug) << endl;
+    }
 }
 
 int main()
@@ -27,13 +36,7 @@ int main()
     // Read in the data
     cout << "Reading in Song Data" << endl;
     vector<Point3D> basePoints = readcsv("song_data.csv");
-    // Run The Code with the same data
+    // Run The Code with a copy of the song data
     vector<Point3D> points1 = basePoints;
     run(25, 6, &points1);
-    // vector<Point3D> points2 = basePoints;
-    // run(200, 6, &points2, 12);
-    // vector<Point3D> points3 = basePoints;
-    // run(400, 6, &points3, 12);
-    // vector<Point3D> points4 = basePoints;
-    // run(800, 6, &points4, 12);
 }
