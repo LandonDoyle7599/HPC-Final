e666ead35d32cb428e22fbf899d3047b04635d86
Max Edwards <maxedwards20@gmail.com>
2023-11-10 13:44:21 -0700

    Adds custom MPI datatype

--- distributedCPU.cpp
+++ distributedCPU.cpp
@@ -22,15 +22,16 @@ void kMeansClusteringCPU(vector<Point3D> *points, vector<Point3D> *centroids, in
 // Define MPI datatype for Point3D
 MPI_Datatype createPoint3DType()
 {
-    const int nitems = 4;                                                  // Number of items in the structure
-    int blocklengths[4] = {1, 1, 1, 1};                                    // Number of elements for each item
-    MPI_Datatype types[4] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_INT}; // Datatypes of each item
+    const int nitems = 5;                                                              // Number of items in the structure
+    int blocklengths[5] = {1, 1, 1, 1, 1};                                             // Number of elements for each item
+    MPI_Datatype types[4] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_INT, MPI_DOUBLE}; // Datatypes of each item
 
     MPI_Aint offsets[4]; // Offsets of each item in the structure
     offsets[0] = offsetof(Point3D, x);
     offsets[1] = offsetof(Point3D, y);
     offsets[2] = offsetof(Point3D, z);
     offsets[3] = offsetof(Point3D, cluster);
+    offsets[4] = offsetof(Point3D, minDist);
 
     MPI_Datatype mpi_point_type;
     MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_point_type);
