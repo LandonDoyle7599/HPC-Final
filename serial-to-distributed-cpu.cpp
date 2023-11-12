7a46b60fed76e3090ca287918be538f2a591ada9
Max Edwards <maxedwards20@gmail.com>
2023-11-11 20:44:49 -0700

    Adds logging

--- serial-to-distributed-cpu.cpp
+++ serial-to-distributed-cpu.cpp
@@ -194,6 +194,19 @@ int main(int argc, char *argv[])
         displacements[i + 1] += 1;
     }
 
+    // Print the send counts and displacements
+    cout << "Displacements will be : " << endl;
+    for (int i = 0; i < displacements.size(); ++i)
+    {
+        cout << displacements[i] << " ";
+    }
+    cout << endl;
+    cout << "Send counts will be : " << endl;
+    for (int i = 0; i < send_counts.size(); ++i)
+    {
+        cout << send_counts[i] << " ";
+    }
+
     MPI_Barrier(MPI_COMM_WORLD);
 
     cout << "Rank : " << world_rank << " scattering x points " << endl;
@@ -202,23 +215,17 @@ int main(int argc, char *argv[])
     MPI_Scatterv(data_x_points.data(), send_counts.data(), displacements.data(), MPI_DOUBLE,
                  recv_x.data(), recv_x.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
     cout << "Rank : " << world_rank << " scattering y points " << endl;
 
     // Scatterv for y points
     MPI_Scatterv(data_y_points.data(), send_counts.data(), displacements.data(), MPI_DOUBLE,
                  recv_y.data(), recv_y.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
 
     cout << "Rank : " << world_rank << " scattering z points " << endl;
 
     // Scatterv for z points
     MPI_Scatterv(data_z_points.data(), send_counts.data(), displacements.data(), MPI_DOUBLE,
                  recv_z.data(), recv_z.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    // cout << "Rank : " << world_rank << " Num Epochs " << numEpochs << endl;
 
     int count = 0;
     auto start = chrono::high_resolution_clock::now();
