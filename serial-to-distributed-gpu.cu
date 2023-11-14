eec0f88da4cd2b400a849a17b283e579406c53cd
Max Edwards <maxedwards20@gmail.com>
2023-11-14 13:22:35 -0700

    We know how to use distributed and serial. Just need to know how to compile this program

--- serial-to-distributed-gpu.cu
+++ serial-to-distributed-gpu.cu
@@ -8,39 +8,9 @@
 #include <chrono>
 #include <mpi.h>
 #include "serial.cpp"
+#include <cuda_runtime.h>
 
 using namespace std;
-
-
-
-__global__ calculateKMean(double k_x[], double k_y[], double k_z[],
-                    double recv_x[], double recv_y[], double recv_z[], int assign[], int numLocalDataPoints, int numCentroids)
-{
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    // Check if we are out of bounds
-    if (i > numLocalDataPoints){
-        return;
-    }
-    double min_dist = numeric_limits<double>::max();
-    int clusterID = 0;
-    for (int j = 0; j < numCentroids; ++j)
-    // Find the closest centroid
-    {
-        double x = abs(recv_x[i] - k_x[j]);
-        double y = abs(recv_y[i] - k_y[j]);
-        double z = abs(recv_z[i] - k_z[j]);
-        double temp_dist = (x * x) + (y * y) + (z * z);
-
-        if (temp_dist < min_dist)
-        {
-            min_dist = temp_dist;
-            clusterID = j;
-        }
-    }
-    // Update the assignment
-    assign[i] = clusterID;
-    
-}
 
 void updateCentroidDataDistributed(double k_means_x[], double k_means_y[], double k_means_z[],
                                    double data_x_points[], double data_y_points[], double data_z_points[], int k_assignment[], int numElements, int numCentroids)
