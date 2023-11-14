a9184521cf4b9f77b468f5de447767730c0eb301
Max Edwards <maxedwards20@gmail.com>
2023-11-14 11:36:39 -0700

    fix

--- serial-to-distributed-cpu.cpp
+++ serial-to-distributed-cpu.cpp
@@ -270,7 +270,7 @@ int main(int argc, char *argv[])
         {
             Point3D p = Point3D(data_x_points[i], data_y_points[i], data_z_points[i]);
             p.cluster = k_assignment[i];
-            computedPoints.push_back(Point3D(data_x_points[i], data_y_points[i], data_z_points[i]));
+            computedPoints.push_back(p);
         }
         saveOutputs(&computedPoints, distFilename);
         printStats(numEpochs, numCentroids, &computedPoints, duration);
