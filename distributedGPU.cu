7139f3d3e31c73522f68dac2bbcc212b09c916ae
Max Edwards <maxedwards20@gmail.com>
2023-11-15 07:33:00 -0700

    Return tyupe void

--- distributedGPU.cu
+++ distributedGPU.cu
@@ -1,7 +1,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cstdlib>
+using namespace std;
 
-__global__ calculateKMean(double k_x[], double k_y[], double k_z[], double recv_x[], double recv_y[], double recv_z[], int assign[], int numLocalDataPoints, int numCentroids){
+__global__ void calculateKMean(double k_x[], double k_y[], double k_z[], double recv_x[], double recv_y[], double recv_z[], int assign[], int numLocalDataPoints, int numCentroids){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     // Check if we are out of bounds
     if (i > numLocalDataPoints){
