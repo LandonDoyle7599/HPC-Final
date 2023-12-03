94072670bd6184fdfafe473ae30a3cac64981c56
Max Edwards <maxedwards20@gmail.com>
2023-12-03 08:24:25 -0700

    Adds command line parsing

--- serial-only.cpp
+++ serial-only.cpp
@@ -1,13 +1,15 @@
 #include "serial.cpp"
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
     // Read in the data
     cout << "Reading the csv" << endl;
     vector<Point3D> points = readcsv("song_data.csv");
-    int numEpochs = 25;
-    int numCentroids = 6;
+    // Get the epochs and centroids from the command line
+    int numEpochs;
+    int numCentroids;
+    getEpochsCentroids(argc, argv, numEpochs, numCentroids);
     // Because this is random initialization we need to share it between the serial and GPU to ensure they are valid
     vector<Point3D> centroids = initializeCentroids(numCentroids, &points);
     string f1 = "serial-cpu.csv";
